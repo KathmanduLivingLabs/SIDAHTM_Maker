957277bc0ef6789bb9c0c975b4ccd2e90e4bd924
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-10-11 18:06:54 +0545

    inital commit

---
+++ fetchData.sh
@@ -0,0 +1,2 @@
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://ona.io/api/v1/forms/68589/form.json >school_label.json
+curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.json >school.json
